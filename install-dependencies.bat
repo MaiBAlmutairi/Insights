37015f615589665342f81f48048c0f870ae24bcd
MaiBAlmutairi <92514901+MaiBAlmutairi@users.noreply.github.com>
2022-02-24 21:20:29 +0300

    Add files via upload

---
+++ install-dependencies.bat
@@ -0,0 +1,22 @@
+npm install --save cordova-plugin-file-transfer
+npm install --save cordova-plugin-document-viewer
+npm install --save @ionic-native/file @ionic-native/document-viewer @ionic-native/file-transfer
+npm install --save cordova-plugin-file-opener2
+npm install --save  @ionic-native/file-opener
+npm install --save cordova-plugin-globalization
+npm install --save @ionic-native/globalization
+npm install --save cordova-plugin-app-version
+npm install --save @ionic-native/app-version
+
+npm install --save cordova-plugin-firebase-analytics
+npm install --save @ionic-native/firebase-analytics
+
+
+npm install --save cordova-plugin-screen-orientation
+npm install --save  @ionic-native/screen-orientation
+
+npm i --save cordova-plugin-ionic-webview
+
+ionic cap sync
+
+
